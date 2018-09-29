559e5d14cc0b353a3ea507e10a27a9979006c263
NajaNaja <NajaNaja89@gmail.com>
2018-09-29 20:05:55 +0600

    logic not done

---
+++ CSharpPractModule8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractModule8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startIndexInt;
+            int endIndexInt;
+
+            Console.WriteLine("Enter start array index ");
+            string startIndex = Console.ReadLine();
+            if (int.TryParse(startIndex, out startIndexInt)) { }
+            else { Console.WriteLine("Value must be a number!"); }
+           
+
+            Console.WriteLine("Enter end array index");
+            string endIndex = Console.ReadLine();
+
+            if (int.TryParse(endIndex, out endIndexInt)) { }
+            else { Console.WriteLine("Value must be a number!"); }
+            if (endIndexInt <= startIndexInt) { Console.WriteLine("end index must be bigger than start index!"); }
+            
+            RangeOfArray array = new RangeOfArray
+            {
+                HighIndexArray = startIndexInt,
+                LowIndexArray = endIndexInt
+            };
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpPractModule8/RangeOfArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpPractModule8
+{
+    public class RangeOfArray
+    {
+        public int LowIndexArray { get; set; }
+        public int HighIndexArray { get; set; }
+        private int size;
+        public int Size
+        {
+            get
+            {
+                size = HighIndexArray - LowIndexArray;
+                return size;
+            }
+   
+        }
+
+
+        
+
+        private string[] customArray;
+        public string this[int index]
+        {   
+            get { return customArray[index]; }
+            set { customArray[index] = value; }
+        }
+    }
+}
